08b8160243e4df75c86abcf7c339ac163a0f0520
Bruce McMoran <mcmoranbjr@gmail.com>
2020-05-06 17:34:34 -0700

    feature(Event): Sort by start time
    Allow events to be ordered by start time with the soonest at the top of the list

--- app/javascript/components/Events.vue
+++ app/javascript/components/Events.vue
@@ -5,6 +5,7 @@
           <md-select v-model='comparatorName' v-on:md-selected='onSelected'>
               <md-option value='orderByCreated'>Date created</md-option>
               <md-option value='orderByLastCommented'>Last commented</md-option>
+              <md-option value='orderByStartTime'>Start time</md-option>
           </md-select>
       </md-field>
       <EventList
@@ -16,6 +17,9 @@ import {getAllEvents} from '../services/events'
 import EventList from './EventList.vue'
 import {parseDate} from '../services/dates'
 import compareDesc from 'date-fns/compareDesc'
+import compareAsc from 'date-fns/compareAsc'
+import isAfter from 'date-fns/isAfter'
+import isBefore from 'date-fns/isBefore'
 
 export default {
     name: 'Events',
@@ -28,6 +32,12 @@ export default {
     },
     computed: {
         events() {
+            if (this.comparatorName === 'orderByStartTime') {
+                let d = new Date()
+                return this.$store.getters.events.filter(e => {
+                    return isAfter(parseDate(e.end_time), d)
+                }).sort(this.comparator)
+            }
             return this.$store.getters.events.sort(this.comparator)
         }
     },
@@ -39,14 +49,19 @@ export default {
             this.comparator = this[v]
         },
         orderByCreated(a, b) {
+            let aTime = parseDate(a.created_at)
+            let bTime = parseDate(b.created_at)
+            return compareDesc(aTime, bTime)
+        },
+        orderByLastCommented(a, b) {
             let aTime = parseDate(a.updated_at)
             let bTime = parseDate(b.updated_at)
             return compareDesc(aTime, bTime)
         },
-        orderByLastCommented(a, b) {
-            let aTime = parseDate(a.last_commented)
-            let bTime = parseDate(b.last_commented)
-            return compareDesc(aTime, bTime)
+        orderByStartTime(a, b) {
+            let aTime = parseDate(a.start_time)
+            let bTime = parseDate(b.start_time)
+            return compareAsc(aTime, bTime)
         }
     }
 }
--- app/javascript/services/dates.js
+++ app/javascript/services/dates.js
@@ -1,11 +1,15 @@
 import parseJSON from 'date-fns/parseJSON'
 import formatRelative from 'date-fns/formatRelative'
 import isValid from 'date-fns/isValid'
+import addMinutes from 'date-fns/addMinutes'
 
+
+// Dates are stored as UTC
 export const parseDate = (date) => {
+    let offset = new Date().getTimezoneOffset()
     let parsed = parseJSON(date)
     if (isValid(parsed)) {
-        return parsed
+        return addMinutes(parsed, offset)
     }
     console.error('Failed to parse date', date)
     return new Date()
